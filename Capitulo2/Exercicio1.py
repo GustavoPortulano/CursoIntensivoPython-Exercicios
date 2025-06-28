07abb7034f72bfe936c2d2192effec33a9aadb5b
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-28 18:57:14 -0300

    Exercicio 2.1, primeiro commit.

---
+++ Capitulo2/Exercicio1.py
@@ -0,0 +1,9 @@
+# 28 de junho de 2025.
+# Sábado, inicio de noite fria
+
+# Exercicio 2.1 - Mensagem simples: Armazene uma mensagem em uma variável e, em
+# seguida, exiba amensagem.
+
+mensagem = "Exercício 2.1. Produzindo a primeira mensagem."
+
+print(mensagem)
