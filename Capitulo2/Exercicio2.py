cc9b65ead4741ede9ec44eaf419068570bcbca7d
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-28 19:14:51 -0300

    Exercicio 2.2, segundo commit

---
+++ Capitulo2/Exercicio2.py
@@ -0,0 +1,13 @@
+# 28 de junho de 2025.
+# Sábado, inicio de noite fria.
+
+# Exercicio 2.2 - Mensagens simples: Armazene uma mensagem em uma variável e, em
+# seguida, exiba essa mesagem. Enão altere o valor de sua variável para uma
+# nova mensgem emostre essa nova mensagem.
+
+mensagem = "Curso intensivo de MYSQL."
+print(mensagem)
+
+# Nova mensagem
+mensagem = "Análise estatística com Excel."
+print(mensagem)
