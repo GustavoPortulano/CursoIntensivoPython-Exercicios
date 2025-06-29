adb6a59b7068d7f7bc1453d0e7b842dd6d41f0b5
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-29 17:28:01 -0300

    Exercicios 6 e 7

---
+++ Capitulo2/Exercicio6.py
@@ -0,0 +1,15 @@
+# 29 de junho de 2025.
+# Domingo, noite fria.
+
+Exercicio6 = """Citação famosa 2: Repita o Exercício 2.5, porém, desta vez, armazene o
+nome da pessoa famosa em uma variável chamada famous_person. Em seguida, componha sua 
+mensagem e armazene-a em uma nova variável chamada message. Exiba sua mensagem"""
+
+famous_person = "Bismark"
+citacao = "Uma pessoa inteligente aprende com os próprios erros. " \
+"Uma pessoa sábia aprende com o erro dos outros."
+
+message = "\nSegundo " + famous_person + ": " + citacao
+
+print(message)
+
---
+++ Capitulo2/Exercicio7.py
@@ -0,0 +1,12 @@
+# 29 de junho de 2025.
+# Domingo, noite fria.
+
+Exercicio7 = """\nExercidio 7 - Removendo caracteres em branco de nomes: Armazene o nome de uma
+pessoa e inclua alguns caracteres em branco no início e no final do nome. Lembre-se
+de usar cada combinação de caracteres, '\ t e \ n', pelo menos uma ves."""
+
+print(Exercicio7)
+
+nome = " Ana "
+print(nome.lstrip())
+print(nome.rstrip())
