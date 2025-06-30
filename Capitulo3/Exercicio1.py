bdcb37f642583c578616a52c1d136696e5d076e7
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-29 21:49:52 -0300

    Exercicios 1 e 2

---
+++ Capitulo3/Exercicio1.py
@@ -0,0 +1,15 @@
+# 29 de junho de 2025
+# Domingo, noite fria
+
+nomes = """\n3.1 - Armazene os nomes de alguns de seus amigos em uma lista 
+chamada names. Exiba o nome de cada pessoa acessando cada elemento  da 
+lista, um de cada vez."""
+
+names = ['ana', 'bella', 'cintia', 'denis']
+
+print(nomes)
+print(names)
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
---
+++ Capitulo3/Exercicio2.py
@@ -0,0 +1,15 @@
+# 29 de junho de 2025
+# Domingo, noite fria
+
+saudacoes = """\n3.2 - Comece com a lista usada no Exercício 3.1, mas em vez de
+simplesmente exibir o nome de cada pessoa, apresente uma mensagem a elas. O
+texto de cada mensagem deve ser o mesmo, porém cada mensagem deve
+estar personalizada com o nome da pessoa."""
+
+names = ['ana', 'bella', 'cintia']
+menssagem = "Bom dia"
+
+print(saudacoes)
+print(menssagem + " " + names[0].title())
+print(menssagem + " " + names[1].title())  
+print(menssagem + " " + names[2].title())
