b894d0928eaec03374affa229557497874a97683
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-29 23:27:16 -0300

    Exercicios 4 e 5

---
+++ Capitulo3/Exercicio4.py
@@ -0,0 +1,14 @@
+# 29 de junho de 2025
+# Domingo, noite fria
+
+lista = """\n3.4 - Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas que você gostaria
+de convidar para jantar. Em seguida, utilize sua lista para exibir uma mensagem para cada pessoa,
+convidando-a para jantar."""
+
+print(lista)
+
+convidados = ['Elvira', 'Diana', 'Nico Robin']
+print(convidados[0] + ", convido você para um jantar hoje a noite!")
+print(convidados[1] + ", convido você para um jantar hoje a noite!")
+print(convidados[2] + ", convido você para um jantar hoje a noite!")
---
+++ Capitulo3/Exercicio5.py
@@ -0,0 +1,6 @@
+# 29 de junho de 2025
+# Domingo, noite fria
+
+lista = """Você acabou de saber que um de seus convidados não poderá 
+comparecer ao jantar, portanto será necessário enviar um novo conjunto
+ de convites. Você deverá pensar em outra pessoa para convidar."""
