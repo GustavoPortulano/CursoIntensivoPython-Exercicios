428cad7f86ba9d68b62a90f2909c903177b2d2f2
GustavoPortulano <gfa.portulano@gmail.com>
2025-07-01 01:20:05 -0300

    Exercicios 5 e 6

--- Capitulo3/Exercicio5.py
+++ Capitulo3/Exercicio5.py
@@ -1,6 +1,32 @@
 # 29 de junho de 2025
 # Domingo, noite fria
 
-lista = """Você acabou de saber que um de seus convidados não poderá 
+lista = """\nVocê acabou de saber que um de seus convidados não poderá 
 comparecer ao jantar, portanto será necessário enviar um novo conjunto
- de convites. Você deverá pensar em outra pessoa para convidar."""
+ de convites. Você deverá pensar em outra pessoa para convidar.
+ * Comece com seu programa do Exercício 3.4 Acrescente uma instrução print
+ no final de seu programa, especificando o nome do convidade que não
+ poderá comparecer.
+ * Modifique sua lista, substituindo o nome de convidado que não poderá
+ comparecer pelo nome da nova pessoa que você está convidando.
+ * Exiba um segundo conjunto de mensagens com o convite, uma para cada
+ pessoa que continua presente em sua lista."""
+
+print(lista)
+
+convidados = ['Elvira', 'Diana', 'Nico Robin']
+print(convidados)
+
+print("\n")
+print(convidados[0] + ", convido você para um jantar hoje a noite!")
+print(convidados[1] + ", convido você para um jantar hoje a noite!")
+print(convidados[2] + ", convido você para um jantar hoje a noite!")
+
+print(convidados[2] + ", por incompatibilidade de agenda, não poderá comparecer ao nosso jantar.")
+
+print("\nNova lista de convidados:")
+novo_convidado = convidados.append('Srta Nani')
+print(convidados[0] + ", convido você para um jantar hoje a noite!")
+print(convidados[1] + ", convido você para um jantar hoje a noite!")
+print(convidados[3] + ", convido você para um jantar hoje a noite!")
+print("\n")
